1c9cf743c351fc471949e7f7e09747f8e8d40a00
hzyangwangshu <Yws123456!>
2016-09-23 09:40:05 +0800

    增加terminal版本jse

--- src/main/sendorder.go
+++ src/main/sendorder.go
@@ -7,6 +7,9 @@ import (
 	"io"
 	"log"
 	"time"
+	"io/ioutil"
+	"github.com/henrylee2cn/pholcus/common/simplejson"
+	"fmt"
 )
 
 func ReadLine(fileName string, handler func([]byte)) error {
@@ -35,7 +38,15 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial(os.Args[0])
+	configData,_ := ioutil.ReadFile("./config.json")
+	fmt.Println(string(configData))
+	configJson,_ := simplejson.NewJson(configData)
+	conn ,err := amqp.Dial(configJson.Get("conn").MustString())
+	file := configJson.Get("file").MustString()
+	exchange := configJson.Get("exchange").MustString()
+	routekey := configJson.Get("routekey").MustString()
+	interval := configJson.Get("sendInterval").MustInt64()
+
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -43,20 +54,31 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"queue.paidorder", // name
-		true, // durable
-		false, // delete when unused
-		false, // exclusive
-		false, // no-wait
-		nil, // arguments
-	)
+	//q, err := ch.QueueDeclare(
+	//	queue, // name
+	//	true, // durable
+	//	false, // delete when unused
+	//	false, // exclusive
+	//	false, // no-wait
+	//	nil, // arguments
+	//)
+	//q, err := ch.QueueDeclare(
+	//	"queue.paidorder", // name
+	//	true, // durable
+	//	false, // delete when unused
+	//	false, // exclusive
+	//	false, // no-wait
+	//	nil, // arguments
+	//)
 	failOnError(err, "Failed to declare a queue")
 
-	ReadLine(os.Args[1], func(line []byte) {
+	ReadLine(file, func(line []byte) {
+		if string(line) == "" {
+			return
+		}
 		err = ch.Publish(
-			"", // exchange
-			q.Name, // routing key
+			exchange, // exchange
+			routekey, // routing key
 			false, // mandatory
 			false, // immediate
 			amqp.Publishing{
@@ -65,8 +87,8 @@ func main() {
 			})
 		log.Printf(" [x] Sent %s", line)
 		failOnError(err, "Failed to publish a message")
-		// 10 msg/second
-		time.Sleep(time.Millisecond*100)
+		// 1000/num 条/秒
+		time.Sleep(time.Millisecond * time.Duration(interval))
 	})
 
 }
